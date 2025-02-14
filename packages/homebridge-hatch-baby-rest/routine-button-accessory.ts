6981f9555f581c9a6002e54e441278cc56e90bae
Daniel King <kingds3272@gmail.com>
2025-02-14 13:12:25 -0700

    WIP

--- packages/homebridge-hatch-baby-rest/routine-button-accessory.ts
+++ packages/homebridge-hatch-baby-rest/routine-button-accessory.ts
@@ -1,28 +1,29 @@
 import { hap } from '../shared/hap'
-import { CharacteristicValue, PlatformAccessory } from "homebridge"
+import { Characteristic, PlatformAccessory } from 'homebridge'
 import { BaseAccessory } from '../shared/base-accessory'
 import { RestIot } from './rest-iot'
-import { Restore } from './restore'
 import { logInfo } from '../shared/util'
 
-
 export class RoutineButtonAccessory extends BaseAccessory {
-  constructor(device : RestIot, accessory: PlatformAccessory) {
-    super(device, accessory);
+  constructor(device: RestIot, accessory: PlatformAccessory) {
+    super(device, accessory)
+
+    logInfo("Creating routine button accessory.")
 
     const { Service, Characteristic } = hap,
       service = this.getService(Service.StatelessProgrammableSwitch)
+    this.registerCharacteristic(
+      service.getCharacteristic(Characteristic.ProgrammableSwitchEvent),
+      device.onSomeContentPlaying,
+      (value) => {
+        logInfo("Value = " + value.toString())
 
-    service.getCharacteristic(Characteristic.ProgrammableSwitchEvent)
-      .onSet(async (value: CharacteristicValue) => {
-        const eventValue = value as number; // Explicitly cast to number
-
-        if (eventValue === Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS) {
+        if (value === Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS) {
           device.nextStep()
-        } else if (eventValue === Characteristic.ProgrammableSwitchEvent.LONG_PRESS) {
-          device.turnOff();
+        } else if (value === Characteristic.ProgrammableSwitchEvent.LONG_PRESS) {
+          device.turnOff()
         }
-      });
+      })
 
     service.setPrimaryService(true)
   }
